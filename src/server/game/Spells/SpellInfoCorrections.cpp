c77e4a3d8bca6a5616d5d279ee3cc721caac1cfa
wang460450180 <460450180@qq.com>
2024-08-10 11:03:35 +0800

    Revert "fix(Scripts/BlackTemple): Mother Shahraz Periodic Beams (#19525)"
    This reverts commit fccfe51594b48520d465b886d6d2b407548b514c.

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4792,12 +4792,6 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->Effects[EFFECT_0].RadiusEntry = sSpellRadiusStore.LookupEntry(EFFECT_RADIUS_20_YARDS);
     });
 
-    // Random Periodic
-    ApplySpellFix({ 40867 }, [](SpellInfo* spellInfo)
-    {
-        spellInfo->Effects[EFFECT_0].Amplitude = 9000;
-    });
-
     for (uint32 i = 0; i < GetSpellInfoStoreSize(); ++i)
     {
         SpellInfo* spellInfo = mSpellInfoMap[i];
