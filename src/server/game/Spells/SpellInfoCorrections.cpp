0983214edeb83d12a686e5794279cbff397eee86
wang460450180 <460450180@qq.com>
2024-08-10 11:02:32 +0800

    Revert "fix(Scripts/BlackTemple): Mother Shahraz Periodic part 2 (#19561)"
    This reverts commit 09e77dfd1c187742d03091e01aac6cad59d302d6.

--- src/server/game/Spells/SpellInfoCorrections.cpp
+++ src/server/game/Spells/SpellInfoCorrections.cpp
@@ -4792,6 +4792,12 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->Effects[EFFECT_0].RadiusEntry = sSpellRadiusStore.LookupEntry(EFFECT_RADIUS_20_YARDS);
     });
 
+    // Random Periodic
+    ApplySpellFix({ 40867 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->Effects[EFFECT_0].Amplitude = 9000;
+    });
+
     // Flame Wave
     ApplySpellFix({ 33800 }, [](SpellInfo* spellInfo)
     {
